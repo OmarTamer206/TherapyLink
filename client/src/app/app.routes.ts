eccc5114dc4e2aa823ff4e863b6385ad1b6f9b9c
OMEGAT206 <131653790+OmarTamer206@users.noreply.github.com>
2025-03-11 21:49:47 +0200

    Update app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ManagerComponent } from './manager/manager.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'manager',
+    component: ManagerComponent,
+  },
+];

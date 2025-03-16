624368ff3262e0257460321425018ac1c5e5f4b9
ShahdSadouma2003 <148314342+ShahdSadouma2003@users.noreply.github.com>
2025-03-16 22:19:23 +0200

    create myChart component for the charts / downland chart.js library / modify myChart component ts /

---
+++ client/src/app/my-chart/my-chart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-my-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-chart.component.html',
+  styleUrl: './my-chart.component.css'
+})
+export class MyChartComponent {
+
+}

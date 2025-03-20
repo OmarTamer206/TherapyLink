ba13bf745b627377be72678e3f1b9715a4b4d81d
OMEGAT206 <131653790+OmarTamer206@users.noreply.github.com>
2025-03-20 22:20:07 +0200

    modification

--- client/src/app/manager/manager-manage-admin-register-page/manager-manage-admin-register-page.component.ts
+++ client/src/app/manager/manager-manage-admin-register-page/manager-manage-admin-register-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-manager-manage-admin-register-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './manager-manage-admin-register-page.component.html',
-  styleUrl: './manager-manage-admin-register-page.component.css'
+  styleUrl: './manager-manage-admin-register-page.component.css',
 })
 export class ManagerManageAdminRegisterPageComponent {
-
+  registerElementClass: string = 'hide';
+  editElementClass: string = '';
+  checkState(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    if (selectedValue === 'register') {
+      this.registerElementClass = '';
+      this.editElementClass = 'hide';
+    } else if (selectedValue === 'edit') {
+      this.registerElementClass = 'hide';
+      this.editElementClass = '';
+    }
+  }
 }

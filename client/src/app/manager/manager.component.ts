6a16cb8473011d4430a9ad2433967ece0aebaa8e
OMEGAT206 <131653790+OmarTamer206@users.noreply.github.com>
2025-03-20 20:41:19 +0200

    fixes

--- client/src/app/manager/manager.component.ts
+++ client/src/app/manager/manager.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-manager',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './manager.component.html',
   styleUrl: './manager.component.css',
 })

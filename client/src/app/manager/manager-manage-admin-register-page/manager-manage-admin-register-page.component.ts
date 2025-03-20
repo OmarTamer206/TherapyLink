ea62d48992cd9464b79e1f0650c0e5017dde7d2d
janasafaa91 <118542716+janasafaa91@users.noreply.github.com>
2025-03-20 22:43:06 +0200

    manage admin
    editing part done

--- client/src/app/manager/manager-manage-admin-register-page/manager-manage-admin-register-page.component.ts
+++ client/src/app/manager/manager-manage-admin-register-page/manager-manage-admin-register-page.component.ts
@@ -10,7 +10,7 @@ import { CommonModule } from '@angular/common';
 export class ManagerManageAdminRegisterPageComponent {
   registerElementClass: string = 'hide';
   editElementClass: string = '';
-  admins: any;
+
   checkState(event: Event) {
     const selectedValue = (event.target as HTMLSelectElement).value;
     if (selectedValue === 'register') {
@@ -20,5 +20,25 @@ export class ManagerManageAdminRegisterPageComponent {
       this.registerElementClass = 'hide';
       this.editElementClass = '';
     }
+  }
+  admins = [
+    { name: 'Youssef Ahmed', id: 1 },
+    { name: 'Ahmed Yasser', id: 2 },
+    { name: 'Sara Fathi', id: 3 },
+    { name: 'Mohamed Ali', id: 4 },
+    { name: 'Khaled Nasser', id: 5 },
+  ];
+
+  // For managing search and filters
+  jobDescriptionFilter = '';
+  registerEditFilter = '';
+
+  onEdit(adminId: number) {
+    console.log('Editing admin with id:', adminId);
+  }
+
+  onDelete(adminId: number) {
+    console.log('Deleting admin with id:', adminId);
+    this.admins = this.admins.filter((admin) => admin.id !== adminId);
   }
 }

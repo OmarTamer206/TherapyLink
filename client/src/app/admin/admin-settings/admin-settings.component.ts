e475838beed31bc30cb0860e25de32ac08ded933
ShahdSadouma2003 <148314342+ShahdSadouma2003@users.noreply.github.com>
2025-03-24 19:32:51 +0200

    add settings in admin components html / make admin-settings components

---
+++ client/src/app/admin/admin-settings/admin-settings.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-settings',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './admin-settings.component.html',
+  styleUrl: './admin-settings.component.css'
+})
+export class AdminSettingsComponent {
+
+}
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -60,7 +60,7 @@ export const routes: Routes = [
       {
         path: 'admin-workforce',
         component: AdminWorkforceComponent,
-      }
+      },
     ],  
   },
   {

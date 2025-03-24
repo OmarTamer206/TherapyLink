ba70ec209ed24a993a3569d7f687683ca30e3e30
janasafaa91 <118542716+janasafaa91@users.noreply.github.com>
2025-03-24 23:10:48 +0200

    log in
    not done

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  goToSignup() {
+    throw new Error('Method not implemented.');
+  }
+  goToLogin() {
+    throw new Error('Method not implemented.');
+  }
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 }

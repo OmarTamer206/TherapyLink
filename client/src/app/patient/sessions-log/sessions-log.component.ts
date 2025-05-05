d1cbe228bd6fcfb0052e4e03f84295150d47cb42
janasafaa91 <118542716+janasafaa91@users.noreply.github.com>
2025-05-05 21:51:36 +0300

    patient

--- client/src/app/patient/sessions-log/sessions-log.component.ts
+++ client/src/app/patient/sessions-log/sessions-log.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sessions-log',
   standalone: true,
   imports: [],
   templateUrl: './sessions-log.component.html',
-  styleUrl: './sessions-log.component.css'
+  styleUrl: './sessions-log.component.css',
 })
 export class SessionsLogComponent {
-
+  constructor(private router: Router) {}
+  goToSessionPage() {
+    this.router.navigate(['patient/session-page']);
+  }
+  goToOldSessionPage() {
+    this.router.navigate(['patient/old-session']);
+  }
 }

2834e86d90997fdb9ac2ca33c96fb98192bf6d1c
ShahdSadouma2003 <148314342+ShahdSadouma2003@users.noreply.github.com>
2025-05-05 22:33:19 +0300

    modifying patient home

--- client/src/app/patient/home/home.component.ts
+++ client/src/app/patient/home/home.component.ts
@@ -1,29 +1,45 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  showTherapistPreference: boolean = false;
+  showTherapistPreference = false;
+  selectedIndex: number | null = null;
+  selectedTherapist: string | null = null; // <-- NEW VARIABLE
 
-  constructor(private router: Router) {}
+  therapists = [
+    { name: 'Clinical Psychologist', image: 'Clinical Psychologist.png' },
+    { name: 'Child and Adolescent Therapist', image: 'Child and Adolescent Therapist.png' },
+    { name: 'Counseling Psychologist', image: 'Counseling Psychologist.png' },
+    { name: 'Couple Therapy', image: 'Couple Therapy.png' },
+    { name: 'Trauma Therapist', image: 'Trauma Therapist.png' },
+  ];
 
   goToChatbot(): void {
-    this.router.navigate(['patient/chatbot']);
+    console.log('Go to Chatbot');
   }
 
   goToDoctorsPage(): void {
-    this.router.navigate(['patient/doctors-page']);
+    console.log('Go to Doctors Page');
   }
 
   goToUpcomingSessionPage(): void {
-    this.router.navigate(['patient/session-page']);
+    console.log('Go to Session Page');
   }
 
   toggleTherapistPreference(): void {
     this.showTherapistPreference = !this.showTherapistPreference;
+  }
+
+  selectTherapist(index: number): void {
+    this.selectedIndex = index;
+    this.selectedTherapist = this.therapists[index].name; // <-- Save selected name
+    console.log('Selected therapist:', this.selectedTherapist);
   }
 }

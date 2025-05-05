2b98deac1349cab9dcb0a1a9ae349cb796e8b3b0
janasafaa91 <118542716+janasafaa91@users.noreply.github.com>
2025-05-05 21:38:37 +0300

    patient

--- client/src/app/patient/life-coach-appointment/life-coach-appointment.component.ts
+++ client/src/app/patient/life-coach-appointment/life-coach-appointment.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-life-coach-appointment',
   standalone: true,
   imports: [],
   templateUrl: './life-coach-appointment.component.html',
-  styleUrl: './life-coach-appointment.component.css'
+  styleUrl: './life-coach-appointment.component.css',
 })
 export class LifeCoachAppointmentComponent {
-
+  constructor(private router: Router) {}
+  goToCheckoutCoachPage() {
+    this.router.navigate(['patient/life-coach-checkout']);
+  }
 }
--- client/src/app/patient/life-coach-page/life-coach-page.component.ts
+++ client/src/app/patient/life-coach-page/life-coach-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-life-coach-page',
   standalone: true,
   imports: [],
   templateUrl: './life-coach-page.component.html',
-  styleUrl: './life-coach-page.component.css'
+  styleUrl: './life-coach-page.component.css',
 })
 export class LifeCoachPageComponent {
-
+  constructor(private router: Router) {}
+  goToMakeAppointmentCoachPage() {
+    this.router.navigate(['patient/life-coach-appointment']);
+  }
 }
